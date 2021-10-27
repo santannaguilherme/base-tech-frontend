c5a1db6e0a4cb65964b2d822937de51376ec0476
Guilherme Sant'Anna <guilherme.sanntana@gmail.com>
2021-10-27 19:55:30 -0300

    alteração das classes scss

--- src/app/baseMenu/component/menu.component.jsx
+++ src/app/baseMenu/component/menu.component.jsx
@@ -1,39 +1,46 @@
-import style from "./menu.modules.scss";
+import style from "./menu.module.scss";
+
 const Menu = () => {
   return (
     <div>
       <nav classname={style.menu}>
-        <ul>
-          <img classname={style.logo} src='../images/BaseTechLogo.png'></img>
-          <li>
-            <a href='#'>Home</a>
+        <ul className={style.menuList}>
+          <img classname={style.logo} src="../images/BaseTechLogo.png"></img>
+          <li className={style.menuListItem}>
+            <a className={style.menuLink} href="#">
+              Home
+            </a>
           </li>
-          <li>
-            <a href='#'>Teste</a>
+          <li className={style.menuListItem}>
+            <a className={style.menuLink} href="#">
+              Teste
+            </a>
           </li>
-          <li>
-            <a href='#'>Forum</a>
+          <li className={style.menuListItem}>
+            <a href="#">Forum</a>
           </li>
-          <li>
-            <a href='#'>Dashboard</a>
+          <li className={style.menuListItem}>
+            <a className={style.menuLink} href="#">
+              Dashboard
+            </a>
           </li>
-          <li>
-            <input type='text' />
+          <li className={style.menuListItem}>
+            <input type="text" />
             <input
               className={style.pesquisa}
-              type='image'
-              src='../images/pesquisa.png'
-              alt='Submit'
-              width='25'
-              height='25'
+              type="image"
+              src="../images/pesquisa.png"
+              alt="Submit"
+              width="25"
+              height="25"
             />
             <input
               className={style.pesquisa}
-              type='image'
-              src='../images/sair.jpg'
-              alt='Submit'
-              width='25'
-              height='25'
+              type="image"
+              src="../images/sair.jpg"
+              alt="Submit"
+              width="25"
+              height="25"
             />
           </li>
         </ul>
